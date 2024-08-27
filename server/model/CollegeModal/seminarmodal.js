9b286bd1a35d5cf9bf2568604bda472441d53a7d
Mpgowtham01 <109659971+Mpgowtham01@users.noreply.github.com>
2024-08-27 10:54:38 +0530

    done

--- server/model/CollegeModal/seminarmodal.js
+++ server/model/CollegeModal/seminarmodal.js
@@ -3,22 +3,22 @@ import Mongoose from "mongoose";
 const { Schema, model } = Mongoose;
 
 const Seminor = new Schema({
-    SeminorTitle: String,
-    technology: Array,
-    time: String,
-    fromdate: String,
-    todate: String,
-    audience: String,
-    contactPerson: String,
-    contactNumber: String,
-    SeminorMode: String,
-    registrationLink: String,
-    about: String,
-    fees: String,
+  SeminorTitle: String,
+  technology: Array,
+  time: String,
+  fromdate: String,
+  todate: String,
+  audience: String,
+  contactPerson: String,
+  contactNumber: String,
+  SeminorMode: String,
+  registrationLink: String,
+  about: String,
+  fees: String,
 });
 
 Seminor.set("autoIndex", true);
 
-const SeminorDb = model("Seminor", Seminor); 
+const SeminorDb = model("Seminor", Seminor);
 
 export default SeminorDb;
